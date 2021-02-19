6093b3019430ae03a3a92c22a80c475f9248b0aa
yizik1980 <syizikm@gmail.com>
2021-02-19 11:29:10 +0200

    fix for the offer computing

--- Client/src/app/site-layout/components/bounch-stairs/bounch-stairs.component.ts
+++ Client/src/app/site-layout/components/bounch-stairs/bounch-stairs.component.ts
@@ -9,23 +9,22 @@ export class BounchStairsComponent implements OnInit {
   bidsRange = new Array<string>();
   notAllowed = new Array<string>();
   @Input()
-  bidBounce:string;
+  bidBounce: string;
   constructor() { }
 
   ngOnInit(): void {
-    if(this.bidBounce){
+    if (this.bidBounce) {
       let decimal = 1;
       let bidBounce = parseFloat(this.bidBounce);
-      while (bidBounce / decimal < 1) { decimal /= 10;}
-      
-      for(let i = 1;i<=4; i++ ){
-        let val = i*parseFloat(this.bidBounce);
+      while (bidBounce / decimal < 1) { decimal /= 10; }
+      for (let i = 1; i <= 4; i++) {
+        let val = i * bidBounce;
         this.bidsRange.push(val.toFixed(2));
-        this.notAllowed.push((bidBounce+i*decimal).toFixed(2));
+        this.notAllowed.push((bidBounce + (i * decimal)).toFixed(2));
       }
-      this.notAllowed.push((bidBounce+5*decimal).toFixed(2));
+      this.notAllowed.push((bidBounce + 5 * decimal).toFixed(2));
     }
-    
+
   }
 
 }
